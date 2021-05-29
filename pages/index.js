fadbdf4c357f86893ad03ed95c91b368d53e1bca
Matthew Pagan <mastapegs01@gmail.com>
2021-05-29 17:31:17 -0400

    installed typescript

--- pages/index.js
+++
@@ -1,7 +0,0 @@
-export default function Home() {
-  return (
-    <>
-      <h1>Web Audio API</h1>
-    </>
-  )
-}
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from "react";
+
+const Home: FC = () => {
+  return (
+    <>
+      <h1>Web Audio API</h1>
+    </>
+  )
+}
+
+export default Home;
