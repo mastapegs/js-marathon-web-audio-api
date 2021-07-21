cba1cdaf344f1fd59f5034749d2a9588d5d4d0a3
Matthew Pagan <mastapegs01@gmail.com>
2021-07-20 21:26:24 -0400

    feat: prepare base structure for mdn example (#6)

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,16 +1,79 @@
-import { FC, useContext, useEffect } from "react";
+import { FC, useContext } from "react";
 import { Header } from "../components/Header";
 import { AudioContext } from "../contexts/AudioContext";
 
+declare global {
+  interface Window {
+    webkitAudioContext?: typeof AudioContext;
+  }
+}
+
 const Home: FC = () => {
-  const { audioContext, setAudioContext } = useContext(AudioContext);
-  useEffect(() => {
-    setAudioContext(new window.AudioContext());
-  }, []);
+  const { audioContext, setAudioContext, volume, setVolume } =
+    useContext(AudioContext);
   return (
     <>
       <Header />
-      {audioContext && console.log(audioContext)}
+      {!audioContext && (
+        <>
+          <div
+            id="container"
+            className="flex justify-center m-4 p-4 border border-gray-200 rounded shadow-lg"
+          >
+            <button
+              onClick={() =>
+                setAudioContext(
+                  new (window.AudioContext || window.webkitAudioContext)()
+                )
+              }
+              type="button"
+              className="px-8 py-4 text-white font-semibold bg-blue-600 rounded shadow"
+            >
+              Start Audio App
+            </button>
+          </div>
+        </>
+      )}
+      {audioContext && (
+        <>
+          <div
+            id="container"
+            className="m-4 p-4 border border-gray-200 rounded shadow-lg"
+          >
+            <div id="keyboard">Keyboard</div>
+            <div id="settingsBar">
+              <div id="left">
+                <span>Volume: </span>
+                <input
+                  type="range"
+                  min="0.0"
+                  max="1.0"
+                  step="0.01"
+                  value={volume}
+                  onChange={(e) => setVolume(Number(e.target.value))}
+                  list="volumes"
+                  name="volume"
+                />
+                <datalist id="volumes">
+                  <option value="0.0" label="Mute" />
+                  <option value="1.0" label="100%" />
+                </datalist>
+              </div>
+              <div id="right">
+                <span>Current Waveform: </span>
+                <select name="waveform">
+                  <option value="sine">Sine</option>
+                  <option value="square">Square</option>
+                  <option value="sawtooth">Sawtooth</option>
+                  <option value="triangle">Triangle</option>
+                  <option value="custom">Custom</option>
+                </select>
+              </div>
+            </div>
+            <p>Volume set to {volume}</p>
+          </div>
+        </>
+      )}
     </>
   );
 };
