03962788bb0141f9100360b5d5b5d37271226fdf
Matthew Pagan <mastapegs01@gmail.com>
2021-07-21 07:27:51 -0400

    create noisebuffer generator

--- components/Drum/Drum.tsx
+++ components/Drum/Drum.tsx
@@ -20,6 +20,22 @@ export const Drum: FC = () => {
     kickDrum.start(now);
     kickDrum.stop(now + 0.5);
   };
+  const noiseBuffer = (): AudioBuffer | null => {
+    if (!audioContext) return null;
+    const bufferSize = audioContext.sampleRate;
+    const buffer = audioContext.createBuffer(
+      1,
+      bufferSize,
+      audioContext.sampleRate
+    );
+    const output = buffer.getChannelData(0);
+
+    for (let i = 0; i < bufferSize; i++) {
+      output[i] = Math.random() * 2 - 1;
+    }
+
+    return buffer;
+  };
   return (
     <div id="drum">
       <button
