0ff44b32db771b95fdbd5aec77e136fe48e947fe
Matthew Pagan <mastapegs01@gmail.com>
2021-07-20 22:02:07 -0400

    finish notetable

--- util/createNoteTable.ts
+++ util/createNoteTable.ts
@@ -46,5 +46,83 @@ export const createNoteTable = (): NoteTable => {
   noteFreq[2]["A#"] = 116.54;
   noteFreq[2]["B"] = 123.47;
 
+  noteFreq[3]["C"] = 130.81;
+  noteFreq[3]["C#"] = 138.59;
+  noteFreq[3]["D"] = 146.83;
+  noteFreq[3]["D#"] = 155.56;
+  noteFreq[3]["E"] = 164.81;
+  noteFreq[3]["F"] = 174.61;
+  noteFreq[3]["F#"] = 185.0;
+  noteFreq[3]["G"] = 196.0;
+  noteFreq[3]["G#"] = 207.65;
+  noteFreq[3]["A"] = 220.0;
+  noteFreq[3]["A#"] = 233.08;
+  noteFreq[3]["B"] = 246.94;
+
+  noteFreq[4]["C"] = 261.63;
+  noteFreq[4]["C#"] = 277.18;
+  noteFreq[4]["D"] = 293.66;
+  noteFreq[4]["D#"] = 311.13;
+  noteFreq[4]["E"] = 329.63;
+  noteFreq[4]["F"] = 349.23;
+  noteFreq[4]["F#"] = 369.99;
+  noteFreq[4]["G"] = 392.0;
+  noteFreq[4]["G#"] = 415.3;
+  noteFreq[4]["A"] = 440.0;
+  noteFreq[4]["A#"] = 466.16;
+  noteFreq[4]["B"] = 493.88;
+
+  noteFreq[5]["C"] = 523.25;
+  noteFreq[5]["C#"] = 554.37;
+  noteFreq[5]["D"] = 587.33;
+  noteFreq[5]["D#"] = 622.25;
+  noteFreq[5]["E"] = 659.25;
+  noteFreq[5]["F"] = 698.46;
+  noteFreq[5]["F#"] = 739.99;
+  noteFreq[5]["G"] = 783.99;
+  noteFreq[5]["G#"] = 830.61;
+  noteFreq[5]["A"] = 880.0;
+  noteFreq[5]["A#"] = 932.33;
+  noteFreq[5]["B"] = 987.77;
+
+  noteFreq[6]["C"] = 1046.5;
+  noteFreq[6]["C#"] = 1108.73;
+  noteFreq[6]["D"] = 1174.66;
+  noteFreq[6]["D#"] = 1244.51;
+  noteFreq[6]["E"] = 1318.51;
+  noteFreq[6]["F"] = 1396.91;
+  noteFreq[6]["F#"] = 1479.98;
+  noteFreq[6]["G"] = 1567.98;
+  noteFreq[6]["G#"] = 1661.22;
+  noteFreq[6]["A"] = 1760.0;
+  noteFreq[6]["A#"] = 1864.66;
+  noteFreq[6]["B"] = 1975.53;
+
+  noteFreq[7]["C"] = 2093.0;
+  noteFreq[7]["C#"] = 2217.46;
+  noteFreq[7]["D"] = 2349.32;
+  noteFreq[7]["D#"] = 2489.02;
+  noteFreq[7]["E"] = 2637.02;
+  noteFreq[7]["F"] = 2793.83;
+  noteFreq[7]["F#"] = 2959.96;
+  noteFreq[7]["G"] = 3135.96;
+  noteFreq[7]["G#"] = 3322.44;
+  noteFreq[7]["A"] = 3520.0;
+  noteFreq[7]["A#"] = 3729.31;
+  noteFreq[7]["B"] = 3951;
+
+  noteFreq[8]["C"] = 4186.0;
+  noteFreq[8]["C#"] = 4434.92;
+  noteFreq[8]["D"] = 4698.64;
+  noteFreq[8]["D#"] = 4978.03;
+  noteFreq[8]["E"] = 5274.04;
+  noteFreq[8]["F"] = 5587.65;
+  noteFreq[8]["F#"] = 5919.91;
+  noteFreq[8]["G"] = 6271.93;
+  noteFreq[8]["G#"] = 6644.88;
+  noteFreq[8]["A"] = 7040.0;
+  noteFreq[8]["A#"] = 7458.62;
+  noteFreq[8]["B"] = 7902.13;
+
   return noteFreq;
 };
