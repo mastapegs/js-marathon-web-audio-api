1b5c6f400ff30c9b7939a076c6289605dfedb05a
Matthew Pagan <mastapegs01@gmail.com>
2021-07-20 21:50:43 -0400

    flesh out first 2 octaves of note table

--- util/createNoteTable.ts
+++ util/createNoteTable.ts
@@ -7,9 +7,31 @@ export const createNoteTable = (): NoteTable => {
     noteFreq[i] = {} as OctaveTable;
   }
 
+  noteFreq[0]["C"] = 16.35;
+  noteFreq[0]["C#"] = 17.32;
+  noteFreq[0]["D"] = 18.35;
+  noteFreq[0]["D#"] = 19.45;
+  noteFreq[0]["E"] = 20.6;
+  noteFreq[0]["F"] = 21.83;
+  noteFreq[0]["F#"] = 23.12;
+  noteFreq[0]["G"] = 24.5;
+  noteFreq[0]["G#"] = 25.96;
   noteFreq[0]["A"] = 27.5;
-  noteFreq[0]["A#"] = 29.135235094880619;
-  noteFreq[0]["B"] = 30.867706328507756;
+  noteFreq[0]["A#"] = 29.14;
+  noteFreq[0]["B"] = 30.87;
+
+  noteFreq[1]["C"] = 32.7;
+  noteFreq[1]["C#"] = 34.65;
+  noteFreq[1]["D"] = 36.71;
+  noteFreq[1]["D#"] = 38.89;
+  noteFreq[1]["E"] = 41.2;
+  noteFreq[1]["F"] = 43.65;
+  noteFreq[1]["F#"] = 46.25;
+  noteFreq[1]["G"] = 49.0;
+  noteFreq[1]["G#"] = 51.91;
+  noteFreq[1]["A"] = 55.0;
+  noteFreq[1]["A#"] = 58.27;
+  noteFreq[1]["B"] = 61.74;
 
   return noteFreq;
 };
