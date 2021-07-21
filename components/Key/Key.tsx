d6efdc86a482e3a685e0377f6e14310413c963d1
Matthew Pagan <mastapegs01@gmail.com>
2021-07-20 23:29:39 -0400

    hooking up logic

--- components/Key/Key.tsx
+++ components/Key/Key.tsx
@@ -1,15 +1,21 @@
 import { FC } from "react";
 
+const notePressed = () => {};
+
 interface KeyProps {
   note: string;
   octave: number;
   freq: number;
 }
 export const Key: FC<KeyProps> = ({ note, octave, freq }) => (
-  <div className="flex h-20 border border-black rounded shadow-lg">
+  <button
+    type="button"
+    onMouseDown={notePressed}
+    className="flex h-20 border border-black rounded shadow-lg"
+  >
     <span className="inline-block w-8 text-center">
       {note}
       <sub>{octave}</sub>
     </span>
-  </div>
+  </button>
 );
